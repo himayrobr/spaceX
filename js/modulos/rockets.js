6d3494bbb7cd07491c1f624ed0fac454c1b685cc
himayrobr <169061000+himayrobr@users.noreply.github.com>
2024-06-29 21:31:14 -0500

    feat: :construction: "poco a poco mejorando"

--- js/modulos/rockets.js
+++ js/modulos/rockets.js
@@ -27,24 +27,15 @@ export async function fetchData(page = 1) {
 }
 
 export function createNameRocketHTML(rockets) {
-    let template = "";
-
-    rockets.forEach(rocket => {
-        template += `
-               <div class="name__Rocket">
-        <h1>${rocket.name}</h1>
-    </div>
-        `;
-    });
-
-    return template;
+    return rockets.map(rocket => `
+        <div class="name__Rocket">
+            <h1>${rocket.name}</h1>
+        </div>
+    `).join('');
 }
 
 export function createRocketHTML(rockets) {
-    let template = "";
-
-    rockets.forEach(rocket => {
-        template += `
+    return rockets.map(rocket => `
         <div class="vistas">
             <div class="rocket">
                 <div class="rockets">
@@ -52,113 +43,112 @@ export function createRocketHTML(rockets) {
                     <p>${rocket.description}</p>
                     <p>The estimated cost per rocket launch:<br> ${rocket.cost_per_launch}</p>
                     <p>The date of the first flight of the rocket:<br> ${rocket.first_flight}</p>
-                    <p> Read more about the rocket: <br> ${rocket.wikipedia}
+                    <p>Read more about the rocket: <a href="${rocket.wikipedia}" target="_blank">${rocket.wikipedia}</a></p>
                 </div>
             </div>
             <div class="apilar">
-            <div class="fila1">
-                <section class="velocimetro">
-                    <div class="velocimetro-container">
-                        <div class="velocimetro-background"></div>
-                        <div class="aguja" id="aguja1"></div>
-                    </div>
-                    <div class="velocimetro-container">
-                        <div class="velocimetro-background"></div>
-                        <div class="aguja" id="aguja2"></div>
-                    </div>
-        </div>
-        <div class="fila2">
-                     <div class="informacion">
-                       <h3>INFORMATION ROCKET<h3/>
-                      <div>
-                       <h3> type:</h3>
-                       <p>${rocket.type}</p>
-                    </div>
-                     <div>
-                       <h3>Rocket in service </h3>
-                       <p>${rocket.active}</p>
-                    </div>
-                      <div>
-                       <h3>Number of stages </h3>
-                       <p>${rocket.stages}</p>
-                    </div>
-                    <div>
-                        <h3>Number of propellants </h3>
-                       <p>${rocket.boosters}</p>
-                    </div>
-                    <div>
-                        <h3>Landing legs</h3>
-                       <p>${rocket.landing_legs.number}</p>
-                    </div>
-                     <div>
-                        <h3>Leg material</h3>
-                       <p>${rocket.landing_legs.material}</p>
-                    </div>
-                    <img  class="ship" src="storage/images/ship.png" alt="">
+                <div class="fila1">
+                    <section class="velocimetro">
+                        <div class="velocimetro-container">
+                            <div class="velocimetro-background"></div>
+                            <div class="aguja" id="aguja1"></div>
+                        </div>
+                        <div class="velocimetro-container">
+                            <div class="velocimetro-background"></div>
+                            <div class="aguja" id="aguja2"></div>
+                        </div>
+                    </section>
+                </div>
+                <div class="fila2">
+                    <div class="informacion">
+                        <h3>INFORMATION ROCKET</h3>
+                        <div>
+                            <h3>Type:</h3>
+                            <p>${rocket.type}</p>
+                        </div>
+                        <div>
+                            <h3>Rocket in service:</h3>
+                            <p>${rocket.active}</p>
+                        </div>
+                        <div>
+                            <h3>Number of stages:</h3>
+                            <p>${rocket.stages}</p>
+                        </div>
+                        <div>
+                            <h3>Number of propellants:</h3>
+                            <p>${rocket.boosters}</p>
+                        </div>
+                        <div>
+                            <h3>Landing legs:</h3>
+                            <p>${rocket.landing_legs.number}</p>
+                        </div>
+                        <div>
+                            <h3>Leg material:</h3>
+                            <p>${rocket.landing_legs.material}</p>
+                        </div>
+                        </div>
+                        <img class="ship" src="storage/images/ship.png" alt="">
                     <div class="informacion2">
-                       <h3>engine information<h3/>
-                      <div>
-                       <h3> type:</h3>
-                       <p>${rocket.engines.type}</p>
+                        <h3>ENGINE INFORMATION</h3>
+                        <div>
+                            <h3>Type:</h3>
+                            <p>${rocket.engines.type}</p>
+                        </div>
+                        <div>
+                            <h3>Maximum power loss:</h3>
+                            <p>${rocket.engines.engine_loss_max}</p>
+                        </div>
+                        <div>
+                            <h3>Engine availability:</h3>
+                            <p>${rocket.engines.layout}</p>
+                        </div>
+                        <div>
+                            <h3>Number of engines:</h3>
+                            <p>${rocket.engines.number}</p>
+                        </div>
+                        <div>
+                            <h3>Stage 1 fuel:</h3>
+                            <p>${rocket.engines.propellant_1}</p>
+                        </div>
+                        <div>
+                            <h3>Stage 2 fuel:</h3>
+                            <p>${rocket.engines.propellant_2}</p>
+                        </div>
                     </div>
-                     <div>
-                       <h3>Maximum power loss </h3>
-                       <p>${rocket.engines.engine_loss_max}</p>
-                    </div>
-                      <div>
-                       <h3>Engine availability </h3>
-                       <p>${rocket.engines.layout}</p>
-                    </div>
-                    <div>
-                        <h3>Number of engines </h3>
-                       <p>${rocket.engines.number}</p>
-                    </div>
-                    <div>
-                        <h3>Stage 1 fuel</h3>
-                       <p>${rocket.engines.propellant_1}</p>
-                    </div>
-                     <div>
-                        <h3>Stage 2 fuel</h3>
-                       <p>${rocket.engines.propellant_2}</p>
-                    </div>
-
-                </section>
+                </div>
             </div>
         </div>
         <section class="valores">
-            <h2>inertial velocity</h2>
+            <h2>Inertial velocity</h2>
             <div class="Bar">
                 <img src="storage/images/Bar.svg" alt="">
                 <h3>7.68 km/s</h3>
             </div>
-            <h2>altitude</h2>
+            <h2>Altitude</h2>
             <div class="Bar">
                 <img src="storage/images/Bar.svg" alt="">
                 <h3>390.0 km</h3>
             </div>
-            <h2>apogee</h2>
+            <h2>Apogee</h2>
             <div class="Bar">
                 <img src="storage/images/Bar.svg" alt="">
                 <h3>404.4 km</h3>
             </div>
-            <h2>perigee</h2>
+            <h2>Perigee</h2>
             <div class="Bar">
                 <img src="storage/images/Bar.svg" alt="">
-                <h3>  389.4 km</h3>
+                <h3>389.4 km</h3>
             </div>
-            <h2>inclination</h2>
+            <h2>Inclination</h2>
             <div class="Bar">
                 <img src="storage/images/Bar.svg" alt="">
                 <h3>51.67º</h3>
             </div>
-            <h2>Range to iss</h2>
+            <h2>Range to ISS</h2>
             <div class="Bar">
                 <img src="storage/images/Bar.svg" alt="">
                 <h3>0.02</h3>
             </div>
         </section>
-        `;
-    });
-
-    return template;
+    `).join('');
 }
